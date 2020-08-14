7beba043dace915bf5c0f3789ab42b7b4c300b8b
chasemoore <moorew13@students.ecu.edu>
2020-08-14 12:25:29 -0400

    Displays 1 image
    Displays 1 image given the index.

--- opencv-test.cpp
+++ opencv-test.cpp
@@ -12,21 +12,99 @@ using namespace std;
 
 int main()
 {
-	Mat image = Mat::zeros(300, 600, CV_8UC3);
-	circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
-	circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
-	imshow("Display Window", image);
+	//change this to your image path
+	String path = "C:/Users/willi/Desktop/Grad/DASC6050/images/*.png";  
+
+	vector<cv::String> fn;
+	glob(path , fn, false);
+	vector<Mat> images;
+
+	//number of png files in images folder
+	size_t count = fn.size(); 
+
+	//make sure we're getting these images
+	//printf("%lu", count);
+
+	for (size_t i = 0; i < count; i++)
+	{
+		images.push_back(imread(fn[i]));
+	}
+	
+	namedWindow("Display window", WINDOW_AUTOSIZE);
+
+	//Display the image in the directory
+	imshow("Display window", images[0]);
 	waitKey(0);
+
+    //TODO: Display multiple images in a display window.
 	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+/**Graveyard
+
+//Mat image = Mat::zeros(300, 600, CV_8UC3);
+	//circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
+	//circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
+	//imshow("Display Window", image);
+	//waitKey(0);
+
+
+
+cv::Mat makeCanvas(std::vector<cv::Mat>& vecMat, int windowHeight, int nRows) {
+    int N = vecMat.size();
+    nRows = nRows > N ? N : nRows;
+    int edgeThickness = 10;
+    int imagesPerRow = ceil(double(N) / nRows);
+    int resizeHeight = floor(2.0 * ((floor(double(windowHeight - edgeThickness) / nRows)) / 2.0)) - edgeThickness;
+    int maxRowLength = 0;
+
+    std::vector<int> resizeWidth;
+    for (int i = 0; i < N;) {
+        int thisRowLen = 0;
+        for (int k = 0; k < imagesPerRow; k++) {
+            double aspectRatio = double(vecMat[i].cols) / vecMat[i].rows;
+            int temp = int(ceil(resizeHeight * aspectRatio));
+            resizeWidth.push_back(temp);
+            thisRowLen += temp;
+            if (++i == N) break;
+        }
+        if ((thisRowLen + edgeThickness * (imagesPerRow + 1)) > maxRowLength) {
+            maxRowLength = thisRowLen + edgeThickness * (imagesPerRow + 1);
+        }
+    }
+    int windowWidth = maxRowLength;
+    cv::Mat canvasImage(windowHeight, windowWidth, CV_8UC3, Scalar(0, 0, 0));
+
+    for (int k = 0, i = 0; i < nRows; i++) {
+        int y = i * resizeHeight + (i + 1) * edgeThickness;
+        int x_end = edgeThickness;
+        for (int j = 0; j < imagesPerRow && k < N; k++, j++) {
+            int x = x_end;
+            cv::Rect roi(x, y, resizeWidth[k], resizeHeight);
+            cv::Size s = canvasImage(roi).size();
+            // change the number of channels to three
+            cv::Mat target_ROI(s, CV_8UC3);
+            if (vecMat[k].channels() != canvasImage.channels()) {
+                if (vecMat[k].channels() == 1) {
+                    cv::cvtColor(vecMat[k], target_ROI, CV_GRAY2BGR);
+                }
+            }
+            else {
+                vecMat[k].copyTo(target_ROI);
+            }
+            cv::resize(target_ROI, target_ROI, s);
+            if (target_ROI.type() != canvasImage.type()) {
+                target_ROI.convertTo(target_ROI, canvasImage.type());
+            }
+            target_ROI.copyTo(canvasImage(roi));
+            x_end += resizeWidth[k] + edgeThickness;
+        }
+    }
+    return canvasImage;
+}
+
+*/
